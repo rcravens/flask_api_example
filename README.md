99b1346ab3d256136b01a72728533a814f7ac116
rcravens <bob.cravens@gmail.com>
2024-02-26 10:23:34 -0600

    initial add

---
+++ README.md
@@ -0,0 +1,23 @@
+# Flask API Example
+
+Visit the application at the following end-points:
+
+- ping: `http://localhost/ping`
+- news: `http://localhost/news`
+- weather: `http://localhost/weather`
+
+I
+Note the ID of the responding container is provided in the "PONG" response.
+
+To scale up the app:
+
+`docker compose up -d --scale app=5 --no-recreate`
+
+To scale it back down the app:
+
+`docker compose up -d --scale app=1 --no-recreate`
+
+
+To quit the app:
+
+`docker compose down`
---
+++ app/app.py
@@ -0,0 +1,68 @@
+import os
+import subprocess
+import time
+
+import requests
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+load_dotenv()
+
+
+def hostname():
+    result = subprocess.run(['hostname'], stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8')
+
+
+def json_response(data):
+    response = {
+        '_version': 'VERSION 1',
+        '_host': hostname(),
+        '_ts': time.time(),
+        'data': data
+    }
+
+    return jsonify(response)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    data = {
+        'msg': 'Hello World!',
+    }
+
+    return json_response(data)
+
+
+@app.route('/ping')
+def ping():
+    return json_response('PONG')
+
+
+@app.route('/news', defaults={'country_name': 'US'})
+@app.route('/news/<country_name>')
+def news(country_name):
+    api_key = os.getenv('NEWSAPI_DOT_ORG_API_KEY')
+    base_url = "https://newsapi.org/v2/top-headlines?"
+    complete_url = base_url + "country=" + country_name + "&apiKey=" + api_key
+    data = requests.get(complete_url)
+
+    return json_response(data.json())
+
+
+@app.route('/weather', defaults={'city_name': 'Chicago,IL,US'})
+@app.route('/weather/<city_name>')
+def weather(city_name):
+    api_key = os.getenv('OPENWEATHERMAP_DOT_ORG_API_KEY')
+    base_url = "https://api.openweathermap.org/data/2.5/weather?"
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    data = requests.get(complete_url)
+
+    return json_response(data.json())
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True, port=8000)
